837df690282b1567b9bd4a971a01d9166354183f
Mark Burkley <mark@burkley.net>
2024-10-03 10:29:36 +0100

    start multi-file

--- project/libsysinfo/sysinfo.cc
+++ project/libsysinfo/sysinfo.cc
@@ -1,18 +1,15 @@
+#include "cpuInfo.h"
 #include "sysInfo.h"
 #include <string.h>
 
-// Test function, TODO delete
-JNIEXPORT jint JNICALL Java_sysInfo_intMethod
+// Example JNI function that returns an int
+JNIEXPORT jint JNICALL Java_sysInfo_intExample
   (JNIEnv *env, jobject obj, jint num) {
    return num * num;
 }
 
-JNIEXPORT jint JNICALL Java_cpuInfo_intMethod
-  (JNIEnv *env, jobject obj, jint num) {
-   return num * num;
-}
-
-JNIEXPORT jstring JNICALL Java_sysInfo_stringMethod
+// Example JNI function that returns a string
+JNIEXPORT jstring JNICALL Java_sysInfo_stringExample
     (JNIEnv *env, jobject obj, jstring string)
 {
     const char *name = env->GetStringUTFChars(string, NULL);
@@ -26,4 +23,30 @@ JNIEXPORT jstring JNICALL Java_sysInfo_stringMethod
     return result;
 }
 
-// int main(){}
+JNIEXPORT jint JNICALL Java_cpuInfo_coreCount
+  (JNIEnv *env, jobject obj, jint num) {
+   //  TODO get actual number of cores from /proc/cpuinfo
+   return 8;
+}
+
+JNIEXPORT jstring JNICALL Java_cpuInfo_getModel
+    (JNIEnv *env, jobject obj)
+{
+    const char *model = "Intel(R) Core(TM) i5-9400F CPU @ 2.90GHz";
+
+    jstring result = env->NewStringUTF(model);
+    return result;
+}
+
+JNIEXPORT jint JNICALL Java_cpuInfo_getBusy
+  (JNIEnv *env, jint core) {
+   //  TODO return the busy percent for a specified core
+   return 25;
+}
+
+JNIEXPORT jint JNICALL Java_cpuInfo_getIdle
+  (JNIEnv *env, jint core) {
+   //  TODO return the idle percent for a specified core
+   return 75;
+}
+
