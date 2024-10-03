cf2e52e6dfa0440e1ba5f3310ea8587d2e2c1567
Mark Burkley <mark@burkley.net>
2024-10-03 08:20:21 +0100

    added string method

--- project/libsysinfo/sysinfo.cc
+++ project/libsysinfo/sysinfo.cc
@@ -1,9 +1,29 @@
 #include "sysInfo.h"
 #include <string.h>
 
+// Test function, TODO delete
 JNIEXPORT jint JNICALL Java_sysInfo_intMethod
   (JNIEnv *env, jobject obj, jint num) {
    return num * num;
 }
 
-int main(){}
+JNIEXPORT jint JNICALL Java_cpuInfo_intMethod
+  (JNIEnv *env, jobject obj, jint num) {
+   return num * num;
+}
+
+JNIEXPORT jstring JNICALL Java_sysInfo_stringMethod
+    (JNIEnv *env, jobject obj, jstring string)
+{
+    const char *name = env->GetStringUTFChars(string, NULL);
+    char msg[60] = "Hello ";
+    jstring result;
+
+    strcat(msg, name);
+    env->ReleaseStringUTFChars(string, name);
+    // puts(msg);
+    result = env->NewStringUTF(msg);
+    return result;
+}
+
+// int main(){}
--- project/sysInfo.java
+++ project/sysInfo.java
@@ -1,11 +1,15 @@
+
 public class sysInfo 
 {
      public native int intMethod(int n);
+     public native String stringMethod(String s);
      public static void main(String[] args)
      {
           System.loadLibrary("sysinfo");
           sysInfo info = new sysInfo();
           int     square = info.intMethod(5);
           System.out.println("intMethod: " + square);
+          String     str = info.stringMethod("test");
+          System.out.println("stringMethod: " + str);
      }
 }
