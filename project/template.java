915cbdf4afe917e144fd7653a7f7e75e1404cca8
Mark Burkley <41842627+mburkley@users.noreply.github.com>
2024-10-18 09:16:03 +0100

    Add CPU info methods (#2)
    * Added methods to get CPU info, make, model, core count
    * Added methods to query elapsed user, system and idle time

--- project/template.java
+++ project/template.java
@@ -1,18 +1,34 @@
-// package cs4421;
-
 public class template 
 {
-     public static void main(String[] args)
-     {
-          System.loadLibrary("sysinfo");
-          sysInfo info = new sysInfo();
-          int     square = info.intExample(5);
-          System.out.println("intExample: " + square);
-          String     str = info.stringExample("test");
-          System.out.println("stringExample: " + str);
+    public static void main(String[] args)
+    {
+        System.loadLibrary("sysinfo");
+        sysInfo info = new sysInfo();
 
-          cpuInfo cpu = new cpuInfo();
-          System.out.println("CPU " + cpu.getModel() + " has "+
-              cpu.coreCount() + " cores");
-     }
+        cpuInfo cpu = new cpuInfo();
+        cpu.read();
+
+        System.out.println("CPU " + cpu.getModel() + " has "+
+        cpu.socketCount() + " sockets each with "+
+        cpu.coresPerSocket() + " cores");
+        System.out.println("l1d="+cpu.l1dCacheSize()+
+        ", l1i="+cpu.l1iCacheSize()+
+        ", l2="+cpu.l2CacheSize()+
+        ", l3="+cpu.l3CacheSize());
+
+
+        while(true)
+        {
+            try {
+                Thread.sleep (1000);
+            }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+
+            cpu.read();
+            System.out.println("core 3 idle="+cpu.getIdleTime(3)+"%");
+        }
+    }
 }
